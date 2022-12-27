1769865892e0d57e666f605ef0294b3e1912c1ee
Diogo Gomes Cardoso <diogogomecardoso@gmail.com>
2022-12-27 20:01:29 +0000

    Continuação de interface Grafica, nada de errado neste codigo!

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,128 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     private static Estacao[]  Estacoes = new Estacao[10];
 
+    private static baseFrame mainFrame;
+
     public static void main(String[] args) {
-        mainFrame mainFrame = new mainFrame(Estacoes);
-
-
-
-
-
+        mainFrame = new baseFrame();
+        Main.mainFrames();
 
 
     }
 
+    /**
+     * INTERFACE GRÁFICA PRINCIPAL, A PARTIR DAQUI O UTILIZADOR CONSEGUE ACEDER A TODOS OS MENUS
+     */
+    public static void mainFrames(){
+        JPanel panel = new JPanel();//NOVO PAINEL
+        panel.setBackground(new Color(64,64,64));
+        panel.setBounds(50,90,180,250);
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+
+        ImageIcon source = new ImageIcon("src\\img\\logoGUI.png");
+        ImageIcon resultat = new ImageIcon(source.getImage().getScaledInstance(100, 41, Image.SCALE_DEFAULT));
+
+
+
+        JLabel picLabel = new JLabel(resultat);
+        picLabel.setBounds(50,0,180,100);
+        mainFrame.add(picLabel);
+
+        mainFrame.add(panel);
+
+
+
+
+        ImageIcon logo = new ImageIcon("src\\img\\logo.png");//Imagem do logo
+        mainFrame.setIconImage(logo.getImage());//Define o logo como o icon da janela
+        mainFrame.getContentPane().setBackground(new Color(166, 237, 128));//Define a Cor de fundo
 
 
 
 
 
+        JButton simuladorTrafego = new JButton();
+        simuladorTrafego.setBounds(20,50, 140, 25);
+        simuladorTrafego.setVisible(false);
+        simuladorTrafego.setText("SIMULADO DE TRAFEGO");
+        simuladorTrafego.setFocusable(false);
+
+        JButton gestaoConflitos = new JButton();
+        gestaoConflitos.setBounds(20,90, 140, 25);
+        gestaoConflitos.setVisible(false);
+        gestaoConflitos.setText("GESTÃO DE CONFLITOS");
+        gestaoConflitos.setFocusable(false);
+
+        JButton painelControlo = new JButton();
+        painelControlo.setBounds(20,95, 140, 25);
+        painelControlo.setVisible(false);
+        painelControlo.setText("PAINEL DE CONTROLO");
+        painelControlo.setFocusable(false);
+
+        JButton naoClicar = new JButton();
+        naoClicar.setBounds(20,40, 140, 25);
+        naoClicar.setVisible(false);
+        naoClicar.addActionListener(e -> {
+            try {
+                Main.naoClicar();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        naoClicar.setText("NÃO CLICAR!");
+        naoClicar.setFocusable(false);
+
+
+        JButton gerarDados = new JButton();
+        gerarDados.setBounds(20,50, 140, 25);
+        gerarDados.addActionListener(e -> Main.gerarDados(Estacoes, simuladorTrafego, gestaoConflitos, painelControlo, naoClicar));
+        gerarDados.setText("GERAR DADOS");
+        gerarDados.setFocusable(false);
+        panel.add(gerarDados);
+        panel.add(simuladorTrafego);
+        panel.add(gestaoConflitos);
+        panel.add(painelControlo);
+        panel.add(naoClicar);
+    }
+
+// Esta Função irá gerar todos os dados necessários para a simulação de um sistema de tráfego de comboios,
+// vai ainda fazer com que os botões dos restantes menus fiquem visiveis após o butão a que ele definido seja primido
+
+    public static void gerarDados(Estacao[] Estacoes, JButton button, JButton button2, JButton button3, JButton button4){
+        String[] Paragens = {"São Bento", "Heroísmo" ,"Campanhã", "Estádio do Dragão", "Fanzeres", "Santo Ovídio","Camara de Gaia" ,"João de Deus", "General Torres", "Trindade"};
+
+        for(int i = 0;i < 10;i++){
+            Estacoes[i] = new Estacao(Paragens[i],10,1);
+
+
+        }
+
+        for(int i = 0;i < 10;i++){
+            Estacoes[i].escolherPassageirosRandom(Estacoes);
+        }
+
+        for(int i = 0;i < 10;i++){
+            System.out.println(Estacoes[i].toString());
+        }
+
+        button.setVisible(true);
+        button2.setVisible(true);
+        button3.setVisible(true);
+        button4.setVisible(true);
+
+    }
+
+    public static void naoClicar() throws IOException {
+        java.awt.Desktop.getDesktop().browse(java.net.URI.create("https://encurtador.com.br/iHQW8"));
+    }
 
 
 }
---
+++ src/baseFrame.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class baseFrame extends JFrame {
+
+    public baseFrame() throws HeadlessException {
+
+        this.setTitle("Sistema de Gestão de Tráfego Ferroviário");//Define o Titulo da janela
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Define que o programa acaba quando o user decide fechar a janela
+        this.setResizable(false);//Não permite ao user mudar o tamanho da janela
+        this.setSize(300, 400);//Comprimento e Largura da Janela
+        this.setVisible(true);//A janela é visivel para o utilizador
+        this.setLayout(null);
+        /*
+        ImageIcon logo = new ImageIcon("src\\img\\logo.png");//Imagem do logo
+        this.setIconImage(logo.getImage());//Define o logo como o icon da janela
+        this.getContentPane().setBackground(new Color(166, 237, 128));//Define a Cor de fundo
+
+         */
+    }
+}
